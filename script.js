1a4d2ed49ac6c63e28ef0138234593b7ea92b418
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-04-06 19:38:19 +0200

    ...

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
 "use strict";
+
+const projectForm = document.querySelector(".new-project-form");
+const projectInput = document.querySelector(".project-input");
+
+class Todo {
+  constructor(title) {
+    this.title = title;
+  }
+
+  getTitle() {
+    return this.title;
+  }
+}
+
+class TodoManager {
+  constructor() {
+    this.todos = [];
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+  }
+
+  getTodos() {
+    return this.todos;
+  }
+}
+
+const todoManager = new TodoManager();
+
+projectForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputValue = projectInput.value;
+
+  if (inputValue === "") return;
+
+  const newTodo = new Todo(inputValue);
+  todoManager.addTodo(newTodo);
+
+  projectInput.value = "";
+
+  console.log(
+    "Current Todos:",
+    todoManager.getTodos().map((todo) => todo.getTitle())
+  );
+});
